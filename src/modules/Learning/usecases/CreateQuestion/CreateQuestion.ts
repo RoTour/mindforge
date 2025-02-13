d1d48a05e6d457572f357da92c7ab64b050671d1
Rotour <rtourne@feelingsports.com>
2025-02-13 15:38:06 +0100

    Learning(creation): auto set question type

--- src/modules/Learning/usecases/CreateQuestion/CreateQuestion.ts
+++ src/modules/Learning/usecases/CreateQuestion/CreateQuestion.ts
@@ -5,7 +5,11 @@ import {
 	type OutputFactory,
 	type UseCase
 } from '$lib/arch/UseCase';
-import type { CreateQuestionDto, Question } from '../../entities/Question';
+import {
+	QuestionAnswerSeparator,
+	type CreateQuestionDto,
+	type Question
+} from '../../entities/Question';
 import * as ICreateQuestionRepository from './repositories/ICreateQuestionRepository';
 
 type Input = InputFactory<
@@ -25,6 +29,14 @@ export const CreateQuestion: UseCase<Input, Output> = (deps) => {
 	const { questionBuilder, callbacks, storeQuestion } = deps;
 	return {
 		execute: async ({ dto }) => {
+			const multipleAnswers = dto.answer.split(QuestionAnswerSeparator).length > 1;
+			if (multipleAnswers) {
+				dto.type = 'MULTIPLE_CHOICES';
+			}
+			const hasOnlyTwoOptions = dto.options?.length === 2;
+			if (hasOnlyTwoOptions) {
+				dto.type = 'TRUE_FALSE';
+			}
 			const question = questionBuilder(dto);
 			await storeQuestion(question);
 			callbacks.forEach((callback) => callback(question));
