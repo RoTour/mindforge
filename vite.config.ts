368c230324a26876d762e2e1c00147467a2315d9
Rotour <rtourne@feelingsports.com>
2025-02-16 10:19:41 +0100

    Infra(pwa): first basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
